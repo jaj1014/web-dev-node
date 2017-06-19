43d3e8834c03384dc09eb9a98c2d5717fc1fa25d
Alex Jackson <jaj1014@gmail.com>
2017-06-19 07:53:20 -0400

    added notes to index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 (function () {
-  'use strict'
-
+  // dumb example script to verify it's working
   const elem = document.createElement('p')
   const text = document.createTextNode('If you can see this, js is compiling and working')
   const p = document.getElementById('test')
